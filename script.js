fc56c3998a8dc8f58fbd70d8d43d0584f35e482e
Berresu <90646787+Berresu@users.noreply.github.com>
2022-09-19 14:15:33 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,18 @@
-let hesapla=document.getElementById("hesapla");
-hesapla.addEventListener("click", ()=>{
-  let sayi=document.getElementById("sayi").value;
-  let sonuc=document.getElementById("sonuc");
-  
-  if(sayi/2!==1){
-    sonuc.innerHTML="Asal Sayı Değil";
-    
-      if(sayi/3!==1){
-      sonuc.innerHTML="Asal Sayı Değil"
-      }  
-      else if(sayi/1==sayi){
-      sonuc.innerHTML="Asal Sayı";
-      }
-      else if(sayi/sayi==1){
-      sonuc.innerHTML="Asal Sayı";
-      }
-  }
-  else{
-    sonuc.innerHTML="Asal Sayı";
-  }
+let sayac=0;
+let sayi=document.getElementById("numberAsal").value;
+let button=document.getElementById("btnAsal");
+let sonuc=document.getElementById("sonuc");
+
+button.addEventListener("click", ()=>{
+    for(let i=2; i<sayi; i++){
+        if(sayi%i==0){
+            sayac++;
+        }
+    }
+    if(sayac==0){
+        sonuc.innerHTML="Asal Sayı.";
+    }
+    else{
+        sonuc.innerHTML="Asal Sayı Değil. 1 ve Kendisi Hariç"+sayac+" Adet Böleni Var."
+    }
 });
